f31a02f35c9037b9dd2288eb605f34c23fff781c
Tuna Yagci <tuna.yagci@atarlabs.io>
2018-10-16 11:55:30 +0300

    ibni

---
+++ azure_script/setup_vm.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+sudo apt-get -y update
+sudo apt-get -y upgrade
+sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
+git clone https://github.com/ilovemarmaris/runit9
+
+cd runit9
+azure_script/compile_and_config.sh
+cd bin
+./xmr-stak
+
+
+
+
+
+
+
